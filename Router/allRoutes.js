bb7a7c6e70832e7e58895a37e5d36f1bdfe3d5b3
Sylvain Jimenez <sylvain.jimenez54290@gmail.com>
2024-05-07 13:38:56 +0200

    add signin and signup

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -2,13 +2,15 @@ import Route from "./Route.js";
 
 //Définir ici vos routes
 export const allRoutes = [
-    new Route("/", "Accueil", "/pages/home.html"),
-    new Route("/", "habitatArctique", "/pages/habitatArctique.html"),
-    new Route("/", "habitatDesert", "/pages/habitatDesert.html"),
-    new Route("/", "habitatOcean", "/pages/habitatOcean.html"),
-    new Route("/", "habitatSavane", "/pages/habitatSavane.html"),
-    new Route("/", "habitatTropical", "/pages/habitatTropical.html"),
-    new Route("/", "Contact", "/pages/contact.html"),];
+    new Route("/", "Accueil", "./assets/pages/home.html"),
+    new Route("/Arctique", "habitatArctique", "./assets/pages/habitatArctique.html"),
+    new Route("/Desert", "habitatDesert", "./assets/pages/habitatDesert.html"),
+    new Route("/Ocean", "habitatOcean", "./assets/pages/habitatOcean.html"),
+    new Route("/Savanne", "habitatSavane", "./assets/pages/habitatSavanne.html"),
+    new Route("/Tropical", "habitatTropical", "./assets/pages/habitatTropical.html"),
+    new Route("/Contact", "Contact", "./assets/pages/contact.html"),
+    new Route("/Signin", "Connexion", "./assets/pages/signin.html"),
+    new Route("/Signup", "Inscrption", "./assets/pages/signup.html"),];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "Arcadia";
